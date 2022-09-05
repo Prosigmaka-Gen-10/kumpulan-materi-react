f6e022665f5b6b7596767d05ee70d228fb6c7525
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-09-05 17:19:29 +0700

    add input radio

--- gen-10-react-routing-and-form-handling/src/App.js
+++ gen-10-react-routing-and-form-handling/src/App.js
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>Selamat Datang!</h1>
 
       <hr />
--- gen-10-react-routing-and-form-handling/src/pages/FormHandling.js
+++ gen-10-react-routing-and-form-handling/src/pages/FormHandling.js
@@ -1,17 +1,62 @@
 import { useState } from "react"
 
 export default function FormHandling () {
-	const [nama, setNama] = useState('')
+	const [nama, setNama] = useState()
+	const [hari, setHari] = useState('rabu')
+	const [gender, setGender] = useState('p')
+
+	function handleInputNama (eventTerserah) {
+		setNama(eventTerserah.target.value)
+	}
 
 	return <>
 		<label>
 			Nama Anda: <br />
-			<input type="text" onChange={(event) => setNama(event.target.value)}/>
+			<input type="text" onChange={handleInputNama} value={nama}/>
 		</label>
 
 		<br />
 		<br />
 
-		{nama}
+		<label>
+			Hari: <br />
+			<select onChange={event => setHari(event.target.value)} value={hari}>
+				<option value="senin">Senin</option>
+				<option value="selasa">Selasa</option>
+				<option value="rabu">Rabu</option>
+				<option value="kamis">Kamis</option>
+				<option value="jumat">Jumat</option>
+			</select>
+		</label>
+
+		<br />
+		<br />
+
+
+		Jenis Kelamin: <br />
+		<label>
+			<input
+				type="radio"
+				value="l"
+				name="gender"
+				onChange={event => setGender(event.target.value)}
+				checked={gender === 'l' ? true : false} />
+				Laki laki
+		</label>
+		&nbsp;&nbsp;
+		<label>
+			<input
+				type="radio"
+				value="p"
+				name="gender"
+				onChange={event => setGender(event.target.value)}
+				checked={gender === 'p' ? true : false} />
+				Perempuan
+		</label>
+
+		<br />
+		<br />
+
+		{gender}
 	</>
 }
