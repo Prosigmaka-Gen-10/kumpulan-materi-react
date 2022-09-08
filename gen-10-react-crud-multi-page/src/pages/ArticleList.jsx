8e24d759244614dd7d8405554cd29e99fcaacc82
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-09-08 12:43:51 +0700

    tambahkan fake api

--- gen-10-react-crud-multi-page/src/pages/ArticleList.jsx
+++ gen-10-react-crud-multi-page/src/pages/ArticleList.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function ArticleList () {
+	const [articles, setArticles] = useState([])
+
+	function getArticles () {
+		fetch('http://localhost:3000/articles')
+			.then(res => res.json())
+			.then(data => {
+				setArticles(data)
+			})
+	}
+
+	useEffect(getArticles, [])
+
 	return <>
 		<h1>Daftar Artikel</h1>
 
@@ -17,24 +30,17 @@ export default function ArticleList () {
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>Presiden Gigit Kuda</td>
-					<td>2022-09-08 11:06</td>
-					<td>
-						<button>Edit</button>
-						&nbsp;
-						<button>Hapus</button>
-					</td>
-				</tr>
-				<tr>
-					<td>Presiden Gigit Formula Sikat</td>
-					<td>2022-09-11 11:06</td>
-					<td>
-						<button>Edit</button>
-						&nbsp;
-						<button>Hapus</button>
-					</td>
-				</tr>
+				{articles.map(article =>
+					<tr>
+						<td>{article.article_title}</td>
+						<td>{article.article_publish_date}</td>
+						<td>
+							<button>Edit</button>
+							&nbsp;
+							<button>Hapus</button>
+						</td>
+					</tr>
+				)}
 			</tbody>
 		</table>
 	</>
