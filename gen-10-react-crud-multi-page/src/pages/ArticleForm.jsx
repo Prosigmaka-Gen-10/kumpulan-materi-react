5b11a6a809c7d1f1ceb050ce30d813cb4dcbaede
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-09-08 14:20:56 +0700

    fungsikan form data

--- gen-10-react-crud-multi-page/src/pages/ArticleForm.jsx
+++ gen-10-react-crud-multi-page/src/pages/ArticleForm.jsx
@@ -1,6 +1,37 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function ArticleForm () {
+	const navigate = useNavigate()
+
+	const [formInput, setFormInput] = useState({
+		article_title: '',
+		article_content: '',
+		article_publish_date: '',
+	})
+
+	function handleInput (event, inputName) {
+		const copyFormInput = {...formInput}
+		copyFormInput[inputName] = event.target.value
+		setFormInput(copyFormInput)
+	}
+
+	async function handleSubmit (event) {
+		event.preventDefault()
+
+		const payload = JSON.stringify(formInput)
+
+		await fetch('http://localhost:3000/articles', {
+			method: 'POST',
+			body: payload,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+
+		navigate('/articles')
+	}
+
 	return <>
 		<h1>Form Artikel</h1>
 
@@ -10,17 +41,33 @@ export default function ArticleForm () {
 
 		<br /><br /><br />
 
-		<form>
+		<form onSubmit={event => handleSubmit(event)}>
 			<label>
 				Judul Artikel <br />
-				<input type="text" />
+				<input
+					type="text"
+					value={formInput.article_title}
+					onChange={event => handleInput(event, 'article_title')} />
 			</label>
 
 			<br /><br />
 
 			<label>
 				Tanggal Publish <br />
-				<input type="date" />
+				<input
+					type="date"
+					value={formInput.article_publish_date}
+					onChange={event => handleInput(event, 'article_publish_date')} />
+			</label>
+
+			<br /><br />
+
+			<label>
+				Konten <br />
+				<textarea
+					value={formInput.article_content}
+					onChange={event => handleInput(event, 'article_content')}
+				></textarea>
 			</label>
 
 			<br /><br />
